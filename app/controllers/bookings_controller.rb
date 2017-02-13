714f8c6c75a9935e579121bc85ae6218180e4a27
Gauthier Levha <levha.g@gmail.com>
2017-02-13 17:08:34 +0100

    index et create en cours dans controller bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+
+	def index
+		@bookings = Booking.all
+	end
+
+	def create
+		@artwork = Artwork.find(params[:artwork_id])
+    @booking = Booking.new(booking_params)
+    @booking.artwork = @artwork
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render "artworks/show"
+    end
+	end
+
+	private
+
+	def booking_params
+    params.require(:booking).permit(:starts_on, :ends_on, :price)
+  end
 end
