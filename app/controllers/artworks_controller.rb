eab0489d2dd2ccaf68d4fe8dd121fee6a1a28e73
Francois Galtie <francois.galtie@gmail.com>
2017-02-13 17:46:53 +0100

    avancement create artworks

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,26 @@
 class ArtworksController < ApplicationController
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.user = @user
+    if @artwork.save
+      redirect_to my_artworks_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def mine
+
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:name, :artist_name, :type, :price,
+    :dimensions, :user_id)
+  end
 end
