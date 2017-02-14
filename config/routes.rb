e19a46473d7cba604d0730e1ab81841e4f010716
Eytan Messika <eytan.messika@edhec.com>
2017-02-14 13:11:14 +0100

    last

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :artworks do
-  get '/contact' => 'pages#contact'
-  get '/about' => 'pages#about'
     resources :bookings
   end
   get 'mine', to: 'artworks#mine', as: :mine
+  get '/contact' => 'pages#contact'
+  get '/about' => 'pages#about'
 end
