693683fca78968864dfb3e2342243c6ab4d8443e
Eytan Messika <eytan.messika@edhec.com>
2017-02-17 09:30:51 +0100

    addingreviewtoprofile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,5 +2,19 @@ class ProfileController < ApplicationController
 
 def index
   @artworks = Artwork.where(user: current_user)
+  @reviews = Review.where(reviewed: current_user)
+  @avgreviews = calculate_avg_reviews
+end
+end
+
+def calculate_avg_reviews
+  somme = 0
+  @reviews.each do |review|
+    somme += review.rating
+end
+if @reviews.length != 0
+  moyenne = somme/(@reviews.length)
+else
+  moyenne = 2
 end
 end
